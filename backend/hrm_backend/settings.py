f1dd3b9845fe2163df28c9f634c6c0bbc90a8a3d
Favaz <favazmohammed.vp@gmail.com>
2024-11-09 16:31:10 +0530

    ignorefile

--- backend/hrm_backend/settings.py
+++ backend/hrm_backend/settings.py
@@ -240,12 +240,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "ecombrocom@gmail.com"
-EMAIL_HOST_PASSWORD = "smja ihld snwn nrlr"
+
 
 
 
